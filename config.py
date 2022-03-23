971467311bdd5a59d8ee7632ce53931a5968b441
Lorena Rocha <lore.bezerra.r@gmail.com>
2022-03-23 18:06:24 -0300

    config dotenv

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
-from contact import email, senha
+from dotenv import load_dotenv
+
+load_dotenv()
 
 SECRET_KEY = 'lorena'
 
@@ -10,5 +12,5 @@ MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 465
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
-MAIL_USERNAME = email
-MAIL_PASSWORD = senha
+MAIL_USERNAME = os.getenv('EMAIL')
+MAIL_PASSWORD = os.getenv('SENHA')
